c6012b44c0d31ac92bdb56c7f5d68e1c4e3beb0a
Li-Ting-Huang <help>
2023-02-03 18:06:38 +0800

    加總和次數

---
+++ bill/sum.py
@@ -0,0 +1,16 @@
+# 數值加總
+sum = 0
+data =[7,4,3,6,25]
+for i, num in enumerate(data):
+    
+    sum = sum + num
+print(sum)
+
+# 計算大於5的數值有幾次
+count = 0 
+data =[7,4,3,6,25]
+for i, num2 in enumerate(data):
+   
+        if num2 > 5: 
+            count = count + 1
+print(count)
